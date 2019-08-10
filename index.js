5435c202868944b931236ab8eaa7a5788860ef1d
dylankbuckley <dbuckley14@icloud.com>
2019-08-10 19:11:25 +0100

    Add port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const https = require('https');
 
 app.get('/cube', function(req, res) {
